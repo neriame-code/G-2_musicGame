739325af8e8142807957ed5c62e9b599ee4d4734
neriame-code <09.nishizaka.03@gmail.com>
2025-10-03 19:05:25 +0900

    version

--- Assets/Program/Play/Notes/Note.cs
+++ Assets/Program/Play/Notes/Note.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private int floatSpeed = 70;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, -floatSpeed * Time.deltaTime, 0);
     }
 }
